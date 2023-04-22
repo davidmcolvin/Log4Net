d66526cbaea0ef422fa8d17e14884268a9a2607f
David M. Colvin <davidmcolvin@gmail.com>
2023-04-21 21:07:00 -0400

    Get log4net to write an error message to the console

--- Log4Net/ConsoleUI/Program.cs
+++ Log4Net/ConsoleUI/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+static void myMain() 
+{
+
+
+  log4net.ILog log = log4net.LogManager.GetLogger("Program.cs");
+
+  Console.WriteLine("Hello, World!");
+
+  log.Error("This is my error message");
+}
+
+myMain();
