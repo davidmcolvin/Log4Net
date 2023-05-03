8deb4e270d7311dad579f36a1671b1925a5e0a8f
David M. Colvin <davidmcolvin@gmail.com>
2023-05-02 22:01:12 -0400

    got database logger working

--- Log4Net/ConsoleUI/Program.cs
+++ Log4Net/ConsoleUI/Program.cs
@@ -7,8 +7,12 @@ static void myMain()
   log4net.ILog log = LogHelper.GetLogger();
 
   Console.WriteLine("Hello, World!");
-
+  log.Debug("Developer: Tutorial was run");
+  log.Info("Maintenance: water pump turned on");
+  log.Warn("Maintenance: the water pump is getting hot");
   log.Error("This is my error message");
+  log.Fatal("This is my error message");
+  log.Fatal("This is my error message2");
 }
 
 myMain();
