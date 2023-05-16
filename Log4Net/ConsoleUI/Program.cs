216240a16f15141a199a1b5a6d43cdcfc11dc3fb
David M. Colvin <davidmcolvin@gmail.com>
2023-05-16 19:55:57 -0400

    Add global property

--- Log4Net/ConsoleUI/Program.cs
+++ Log4Net/ConsoleUI/Program.cs
@@ -23,7 +23,16 @@ static void myMain()
     log.Error("Developer: we tried to divide by zero again");
   }
 
-  log.Fatal("h");
+  Counter j = new Counter();
+
+  log4net.GlobalContext.Properties["Counter"] = j;
+
+  for (j.LoopCounter = 0; j.LoopCounter < 5; j.LoopCounter++)
+  {
+    log.Fatal("This is a fatal error in the process" );
+  }
+
+  //log.Fatal("Maintenance: water pump exploded");
 }
 
 myMain();
